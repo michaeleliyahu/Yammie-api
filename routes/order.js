2eea2cad50936f14aac1e3e62c4d2b5eb861d24b
michaeleliyahu <mmyy605@gmail.com>
2022-09-20 11:09:16 +0300

    Install http-error. handlig with Save post request using http-error

--- routes/order.js
+++ routes/order.js
@@ -1,13 +1,20 @@
 const router = require("express").Router();
 const Order = require("../models/Order");
+const CreateError = require("http-errors");
 
-router.post("/", async (req,res) => {
-    const newOrder = new Order(req.body);
+router.post("/", async (req, res, next) => {
     try{
+        const newOrder = new Order(req.body);
         const savedOrder = await newOrder.save();
         res.status(200).json(savedOrder);
     }catch(err){
-        res.status(500).json(err);
+        console.log(err.message);
+        if(err.name === 'validationError')
+        {
+          next(CreateError(422, err.message))
+          return
+        }
+        next(err)
     }
 });
 
@@ -16,6 +23,7 @@ router.get("/", async (req, res) => {
     try {
       const order = await Order.find();
       res.status(200).json(order);
+      console.log("Getting list of all Order");
     } catch (err) {
       res.status(500).json(err);
     }
